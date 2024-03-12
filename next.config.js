c673a9135bf884cdad8a3e4398cfe99b14c0788c
Trevor Sharp <sharp.trevor@me.com>
2024-03-12 23:28:42 +0000

    Add redirect from all routes to main page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  async redirects() {
+    return [
+      {
+        source: "/:any",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default config;
