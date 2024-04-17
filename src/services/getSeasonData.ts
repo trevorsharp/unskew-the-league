765b848ff6d3d7d111ceab19eb2f0e4e87f874ea
Trevor Sharp <sharp.trevor@me.com>
2024-04-17 15:24:56 +0000

    Improve data fetching error handling / Hardcode 2024 season data

--- src/services/getSeasonData.ts
+++ src/services/getSeasonData.ts
@@ -3,26 +3,30 @@ import seasonData from "~/seasonData";
 import { currentSeason, seasonDataSchema } from "~/types";
 import { getSeasonName } from "~/utilities";
 import type { AllSeasonData } from "~/types";
+import { revalidateTag } from "next/cache";
 
 const getLocalSeasonData = (season: number) => seasonData[season];
 
-const errorMessage = (season: number) => `Could not find NBA rankings for ${getSeasonName(season)}`;
+const getError = (season: number) => () => {
+  console.log(`Could not find NBA rankings for ${getSeasonName(season)}`);
+  revalidateTag(`standings-${season}`);
+  return undefined;
+};
 
 const fetchSeasonData = async (season: number) => {
+  const error = getError(season);
+
   try {
     const hoursToCache = season === currentSeason ? 6 : 30 * 24;
 
     const basketballReferenceHtml = await fetch(
       `https://www.basketball-reference.com/leagues/NBA_${season}_standings.html`,
-      { next: { revalidate: hoursToCache * 60 * 60 } },
+      { next: { revalidate: hoursToCache * 60 * 60, tags: [`standings-${season}`] } },
     )
       .then((res) => res.text())
       .catch(() => undefined);
 
-    if (!basketballReferenceHtml) {
-      console.error(errorMessage(season));
-      return undefined;
-    }
+    if (!basketballReferenceHtml) return error();
 
     const expandedStandingsTable = basketballReferenceHtml
       .match(/<table.*?id="expanded_standings"(.|\s)*?<\/table>/gim)
@@ -31,24 +35,17 @@ const fetchSeasonData = async (season: number) => {
       ?.replace(/<tr.*?class="over_header"(.|\s)*?<\/tr>/gim, "")
       ?.replaceAll("<th s", "<td s");
 
-    if (!expandedStandingsTable) {
-      console.error(errorMessage(season));
-      return undefined;
-    }
+    if (!expandedStandingsTable) return error();
 
     const seasonDataResult = seasonDataSchema.safeParse(
       tableToJson.convert(expandedStandingsTable)?.find(() => true),
     );
 
-    if (!seasonDataResult.success) {
-      console.error(errorMessage(season));
-      return undefined;
-    }
+    if (!seasonDataResult.success) return error();
 
     return seasonDataResult.data;
   } catch {
-    console.error(errorMessage(season));
-    return undefined;
+    return error();
   }
 };
 
@@ -60,7 +57,7 @@ const getAllSeasonData = async (seasons: number[]) => {
 
   const seasonDataTasks = seasons.map(async (season) => {
     const seasonData = await getSeasonData(season);
-    allSeasonData[season] = seasonData;
+    if (seasonData) allSeasonData[season] = seasonData;
   });
 
   await Promise.allSettled(seasonDataTasks);
